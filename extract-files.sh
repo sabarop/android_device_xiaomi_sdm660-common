d5c2d795449fb2f7d1032d6c8449e4c246265a8a
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-10-28 23:30:40 +0800

    sdm660-common: Add shim for missing symbol in lib-imsvt.so
    Change-Id: I45961db12f5a1b378e1a59685fb388bc18222e02
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- extract-files.sh
+++ extract-files.sh
@@ -73,6 +73,9 @@ function blob_fixup() {
         system_ext/etc/permissions/qcrilhook.xml)
             sed -i 's|/product/framework/qcrilhook.jar|/system_ext/framework/qcrilhook.jar|g' "${2}"
             ;;
+        system_ext/lib64/lib-imsvideocodec.so)
+            grep -q "libshim_imsvt.so" "${2}" || "${PATCHELF}" --add-needed "libshim_imsvt.so" "${2}"
+            ;;
         system_ext/lib64/libdpmframework.so)
             for LIBSHIM_DPMFRAMEWORK in $(grep -L "libcutils_shim.so" "${2}"); do
                 "${PATCHELF}" --add-needed "libcutils_shim.so" "$LIBSHIM_DPMFRAMEWORK"
