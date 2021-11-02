2a21470977ce3501ffc15a7d3223a3846b0caddf
Mathieu Chartier <mathieuc@google.com>
2021-11-02 21:34:43 +0530

    sdm660-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- sdm660.mk
+++ sdm660.mk
@@ -376,6 +376,9 @@ PRODUCT_PACKAGES += \
     rild \
     librmnetctl
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
