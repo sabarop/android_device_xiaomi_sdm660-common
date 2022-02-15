c8f1a527dc11e46cfb4bc13952f39b8c49871d1b
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-15 11:46:54 -0500

    sdm660-common: switch to retrofit dynamic partitioning
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -458,6 +458,10 @@ PRODUCT_PACKAGES += \
     tune2fs_ramdisk \
     resize2fs_ramdisk
 
+# Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
