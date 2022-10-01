dca937334d14aa63547baf1d1ff06a9f25083314
sabarop <sabar.pakpahan@gmail.com>
2022-10-01 22:24:56 +0700

    sdm660-common: Build Lineage Trust

--- sdm660.mk
+++ sdm660.mk
@@ -408,6 +408,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.xiaomi_sdm660.qti
 
+# Trust
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
