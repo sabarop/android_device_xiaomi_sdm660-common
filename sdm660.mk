27b9492945af8c5bca6fab5b1e1ee8790a3da629
johnmart19 <johnivan19999@gmail.com>
2023-02-26 03:05:57 +0800

    sdm660-common: device: Update audio targets from CAF
    android.hardware.audio@2.0 - 6.0
    android.hardware.audio.effect@2.0 - 6.0
    android.hardware.soundtrigger@2.1 - 2.2

--- sdm660.mk
+++ sdm660.mk
@@ -81,6 +81,18 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libvolumelistener\
 
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl
+
 # Audio Configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
