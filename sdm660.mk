29edb1fc4ef2f400b51a41d71dd2b03d1ae63168
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    sdm660-common: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- sdm660.mk
+++ sdm660.mk
@@ -280,6 +280,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_sdm660
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay native
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
