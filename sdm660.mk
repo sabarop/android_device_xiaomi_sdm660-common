599b97cc934a588baca0656e327d0e680764b534
Sabar <31942277+sabarop@users.noreply.github.com>
2023-07-20 20:27:32 +0700

    sdm660-common: Go lineage

--- sdm660.mk
+++ sdm660.mk
@@ -12,7 +12,7 @@ PRODUCT_COMPRESSED_APEX := false
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
