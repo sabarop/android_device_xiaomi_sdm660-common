ca4b0726db924821d8a55affc13a997d9e5df85b
Sabar <31942277+sabarop@users.noreply.github.com>
2023-11-16 10:08:31 +0700

    sdm660-common: Remove offending entries

--- sdm660.mk
+++ sdm660.mk
@@ -175,8 +175,6 @@ WITH_DEXPREOPT_DEBUG_INFO := false
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.graphics.allocator@3.0-impl \
-    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
@@ -561,7 +559,6 @@ PRODUCT_PACKAGES += \
 # Tin
 PRODUCT_PACKAGES += \
     libtinyxml \
-    tinyxml2 \
     libxml2 
 
 # Thermal
