de898a7bbd4c9b7b83c30dd9a5a176f843a2a553
Sabar <31942277+sabarop@users.noreply.github.com>
2023-11-21 09:16:43 +0700

    sdm660-common: rootdir: Use metadata_csum for /metadata
    This enhances ext4 stability.
    
    Co-Authored-By: Jaegeuk Kim <20465538+jaegeuk@users.noreply.github.com>

--- sdm660.mk
+++ sdm660.mk
@@ -415,7 +415,13 @@ PRODUCT_PACKAGES += \
 
 # RRO configuration
 TARGET_USES_RRO := true
-    
+
+# Partitions
+PRODUCT_PACKAGES += \
+    e2fsck_ramdisk \
+    tune2fs_ramdisk \
+    resize2fs_ramdisk
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
