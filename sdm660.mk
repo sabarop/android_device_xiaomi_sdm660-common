3aa87cc50cc759e3e4cd1ca5172af08ce338d271
Sabar <31942277+sabarop@users.noreply.github.com>
2023-12-07 09:00:28 +0700

    sdm660-common: build livedisplay HAL

--- sdm660.mk
+++ sdm660.mk
@@ -330,6 +330,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service-sysfs
+
 # Logging
 SPAMMY_LOG_TAGS := \
     MiStcImpl \
