6ccfc197bea60cdb01e0f6a914ea209ff8ad02ad
Tim Zimmermann <tim@linux4.de>
2023-12-25 01:57:50 +0800

    sdm660-common: device: Enable support for picture-in-picture
    Change-Id: If0a63fc7e8455100128fa25955bb4da485fff873

--- sdm660.mk
+++ sdm660.mk
@@ -513,6 +513,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.webview.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.webview.xml \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
+    frameworks/native/data/etc/android.software.picture_in_picture.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.picture_in_picture.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 
