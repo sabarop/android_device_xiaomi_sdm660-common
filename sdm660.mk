cde99c4f064b50f398e246248735804b6e37a990
1582130940 <19632674+1582130940@users.noreply.github.com>
2024-02-01 19:43:54 +0800

    sdm660-common: configs: Adapt powerhint to overclock sdm660 freq
    * Remove powerhint_636.json

--- sdm660.mk
+++ sdm660.mk
@@ -481,13 +481,8 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 # Powerhint
-ifeq ($(EAS_POWERHINT_VARIANT), sdm636)
-    PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/sdm636_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-else
-    PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/sdm660_powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-endif
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += \
