13f855bf9236efdcc005acccb9e108b151e135b7
Sabar <31942277+sabarop@users.noreply.github.com>
2024-02-22 14:33:18 +0700

    sdm660-common: Enable Userspace Reboot
    Test: adb reboot userspace
    Co-Authored-By: 0xCAFEBABE <87921983+me-cafebabe@users.noreply.github.com>

--- sdm660.mk
+++ sdm660.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Userspace Reboot
+$(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
