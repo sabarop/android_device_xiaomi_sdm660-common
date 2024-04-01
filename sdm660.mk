c5ddc820ed41f94b2bc779740716a77462704a74
Sabar <31942277+sabarop@users.noreply.github.com>
2024-04-01 17:44:29 +0700

    sdm660-common: device: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Co-Authored-By: Tim Zimmermann <43107815+linux4@users.noreply.github.com>

--- sdm660.mk
+++ sdm660.mk
@@ -7,6 +7,8 @@
 # Userspace Reboot
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
