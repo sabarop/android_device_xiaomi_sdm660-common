491d36cf4312f62c870b383b821dec681b49ad18
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2024-05-07 18:11:31 +0800

    sdm660-common: device: Set TARGET_BOOTANIMATION_HALF_RES to true
    * No apparent resolution difference
    * Much smoother animation
    * Much faster boot
    * What's not to like?
    
    Change-Id: I2119fe826146a3103da50b4862ad88f5950a97ec

--- sdm660.mk
+++ sdm660.mk
@@ -128,6 +128,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Bootanimation
+TARGET_BOOTANIMATION_HALF_RES := true
+
 # Camera
 PRODUCT_PACKAGES += \
     Aperture \
