905e4fafa0a0a99a4aa56ce022b72ac3d2ed4ed5
ROMSG <romsg85@gmail.com>
2024-05-13 20:27:26 -0300

    sdm660-common: Set the compiler filter for shared apks to quicken Change-Id: I8ed63116b929920f604cd1d64110fef95744e425

--- sdm660.mk
+++ sdm660.mk
@@ -153,9 +153,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDolby
 
-# Dex
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
