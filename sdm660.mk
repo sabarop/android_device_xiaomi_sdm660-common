3b8c16084f9ea9246c407d25f321dcd8e96b5a21
basamaryan <basam.aryan@gmail.com>
2024-05-30 00:31:13 +0530

    sdm660-common: Build XiaomiDolby
    Change-Id: I31ff2c27a479423951c8a645bf1cd65dc6ad6f81
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -133,6 +133,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Device settings
+PRODUCT_PACKAGES += \
+    XiaomiDolby
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
