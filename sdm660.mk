5cd9771bc731d554c9c09b72a2a631e92d4c608a
basamaryan <basam.aryan@gmail.com>
2024-05-30 00:31:13 +0530

    sdm660-common: Build XiaomiDolby
    Change-Id: I31ff2c27a479423951c8a645bf1cd65dc6ad6f81
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -667,7 +667,8 @@ PRODUCT_PACKAGES += \
 
 # DeviceSettings
 PRODUCT_PACKAGES += \
-    DeviceSettings
+    DeviceSettings \
+    XiaomiDolby
 
 # Sign build
 -include vendor/lineage-priv/keys/keys.mk
