3896b8ebcb23289ca744410bb308101a1b58c4a1
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    sdm660-common: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- sdm660.mk
+++ sdm660.mk
@@ -476,5 +476,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
