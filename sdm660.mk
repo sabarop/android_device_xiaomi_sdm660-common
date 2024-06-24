4be1064ec55798071f0920ccdd6d019f821a0bf5
ZVNexus <akoskovich@pm.me>
2024-06-24 12:38:14 +0000

    sdm660-common: Disable the usage of ConfigStore
     * As per commit LineageOS/android_hardware_interfaces@11a113a ("Add disable_configstore").
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- sdm660.mk
+++ sdm660.mk
@@ -124,6 +124,10 @@ PRODUCT_PACKAGES += \
     libpng.vendor:32 \
     vendor.qti.hardware.camera.device@1.0:64
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Consumer IR
 ifeq ($(BOARD_HAVE_IR),true)
 PRODUCT_PACKAGES += \
