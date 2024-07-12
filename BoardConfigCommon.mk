5f21b0f6b931397e8b83cf4c854404befbc67483
Sabar <31942277+sabarop@users.noreply.github.com>
2024-07-12 15:56:32 +0700

    sdm660-common: Inherit common partition reserved sizes
    Co-Authored-By: Michael Bestas <mikeNG@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,6 +125,9 @@ endif
 BOARD_SUPER_PARTITION_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_CUST_DEVICE_SIZE) + $(BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE) + $(BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE))
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_SIZE) - 4194304 )
 
+# Partitions - reserved size
+-include vendor/lineage/config/BoardConfigReservedSize.mk
+
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/dsp:/dsp \
     /vendor/firmware_mnt:/firmware \
