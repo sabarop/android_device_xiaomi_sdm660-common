9af6cc21ed3bd4162a2dd122ed7f8eee533c1eeb
Le Hong Duc <hongducthbk123@gmail.com>
2024-07-19 10:57:01 +0700

    sdm660-common: Initialize Evolution X

--- sdm660.mk
+++ sdm660.mk
@@ -7,7 +7,8 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
