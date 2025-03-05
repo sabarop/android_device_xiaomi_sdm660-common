d7077c10cc62c643fc5dae31b93857ed38d6cfde
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2025-03-06 01:21:20 +0530

    sdm660-common: Include Dolby Atmos
    * Using dolby from: https://github.com/crdroidandroid/android_hardware_dolby
    [1]: sm8350-common: Add dolby volume listener effect
    [2]: sm8350-common: audio: Bring all stream effects
    
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -58,6 +58,9 @@ PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 endif
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
