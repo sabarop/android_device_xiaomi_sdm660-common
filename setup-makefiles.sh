8936f712a39a012783112fddf701ee96d6b8a5f3
sabarop <sabar.pakpahan@gmail.com>
2025-03-16 22:24:29 +0700

    sdm660-common: Add vendor soong imports to extract
    Co-Authored-By: webgeek1234 <1706847+webgeek1234@users.noreply.github.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,6 +22,21 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/xiaomi/sdm660-common",
+		"hardware/qcom-caf/common/libqti-perfd-client",
+		"hardware/qcom-caf/sdm660",
+		"hardware/qcom-caf/wlan",
+		"hardware/xiaomi",
+		"vendor/qcom/opensource/commonsys/display",
+		"vendor/qcom/opensource/commonsys-intf/display",
+		"vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um",
+		"vendor/qcom/opensource/dataservices",
+		"vendor/qcom/opensource/display",
+EOF
+}
+
 # Initialize the helper for common
 setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
