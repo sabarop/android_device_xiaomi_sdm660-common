3c5b689be1f178ad6188af0efbb56b9f227eea74
sabarop <sabar.pakpahan@gmail.com>
2025-03-16 11:45:03 +0700

    sdm660-common: Remove obsolete metadata symlink

--- Android.mk
+++ Android.mk
@@ -24,14 +24,6 @@ INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
 
 include $(CLEAR_VARS)
 
-METADATA_SYMLINKS := $(TARGET_ROOT_OUT)/metadata
-$(METADATA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating $@"
-	@mkdir -p $(TARGET_ROOT_OUT)/metadata
-	$(hide) ln -sf /data/apex $@/apex
-
-ALL_DEFAULT_INSTALLED_MODULES += $(METADATA_SYMLINKS)
-
 subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
