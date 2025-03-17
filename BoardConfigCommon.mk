50a62023f13dc62567ae121b02c81b6d804cede6
sabarop <sabar.pakpahan@gmail.com>
2025-03-17 16:01:24 +0700

    sdm660-common: Fix ELF check failures
    Co-Authored-By: CHRISL7 <70589286+chrisl7@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,9 +29,6 @@ else
 AB_OTA_UPDATER := false
 endif
 
-# Broken Files/Headers
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
