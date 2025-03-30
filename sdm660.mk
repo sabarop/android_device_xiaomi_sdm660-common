48b773c158a65821207e95206617519689d509ef
Alcatraz323 <alcatraz32323@gmail.com>
2025-03-30 22:27:59 +0800

    sdm660-common: Implement CAFIMSAdapter
    * As some legacy soc does not support dual volte, we need to notify ims when data sub changed,
      this was done in qti-telephony-common.jar on stock.
    
      May also affect 8996, 8937, etc.
    
    Test: VoLTE works fine after exchanging mobile data networks
    Change-Id: Id958e16f94059eb9fd2ebc6ecc8f33deee17bd61

--- sdm660.mk
+++ sdm660.mk
@@ -114,6 +114,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# CAFIMSAdapter
+PRODUCT_PACKAGES += \
+    CAFIMSAdapter
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
