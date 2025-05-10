1dd780fde5d50a68351627fd52317c05d283bcb9
sabarop <sabar.pakpahan@gmail.com>
2025-05-10 08:35:31 +0700

    sdm660-common: Build XiaomiDolby

--- sdm660.mk
+++ sdm660.mk
@@ -747,7 +747,8 @@ PRODUCT_PACKAGES += \
 
 # DeviceSettings
 PRODUCT_PACKAGES += \
-    DeviceSettings
+    DeviceSettings \
+    XiaomiDolby
 
 # Sign build
 -include vendor/lineage-priv/keys/keys.mk
