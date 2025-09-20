02a669eb7302960a8ec2821680bb489fdf991cc1
sabarop <sabar.pakpahan@gmail.com>
2025-09-20 12:26:42 +0700

    sdm660-common: Switch to AIDL camera HAL
    Co-Authored-By: Tim Zimmermann <43107815+linux4@users.noreply.github.com>

--- sdm660.mk
+++ sdm660.mk
@@ -117,8 +117,7 @@ TARGET_BOOTANIMATION_SIZE := 1080p
 PRODUCT_PACKAGES += \
     Aperture \
     android.hardware.camera.device@3.5 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider-service_32.lineage \
     android.hardware.camera.provider@2.6 \
     libcamera_metadata.vendor \
     libexif.vendor \
