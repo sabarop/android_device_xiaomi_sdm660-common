5548622f14306bc7580b062c44c4b5b839654864
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    sdm660-common: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: I0bf7eafbeb19957326de04ecf7b903580315dc59
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -81,6 +81,8 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
     sound_trigger.primary.sdm660
 
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 # Audio Configs
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(COMMON_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
