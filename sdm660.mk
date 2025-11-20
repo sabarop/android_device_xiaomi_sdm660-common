5dcc39cd5d6772348b134fa03e36a74601c50219
sabarop <sabar.pakpahan@gmail.com>
2025-11-20 15:41:23 +0700

    sdm660-common: clean up config

--- sdm660.mk
+++ sdm660.mk
@@ -612,8 +612,6 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := \
     frameworks/base/boot/boot-image-profile-extra.txt
 
 # Reduce system image size by limiting debug info
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 WITH_DEXPREOPT_DEBUG_INFO := false
 USE_DEX2OAT_DEBUG := false
 
