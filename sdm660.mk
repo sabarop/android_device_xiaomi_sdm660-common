df3093f1843d45922188eaf4b95f3a882bd7a25a
sabarop <sabar.pakpahan@gmail.com>
2025-11-30 07:00:09 +0700

    sdm660-common: import blobs from hon660

--- sdm660.mk
+++ sdm660.mk
@@ -384,6 +384,7 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libc2dcolorconvert \
+    libmm-omxcore \
     libOmxCore \
     libOmxVdec \
     libOmxVenc \
