f0e2df65bea99b7ad86f283ec9936817143ad78a
sabarop <sabar.pakpahan@gmail.com>
2026-04-16 08:43:54 +0700

    Reapply "sdm660-common: adapt to config fragments"
    This reverts commit 14831d73d374c2e591d20200b1a7605f62468eb8.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,6 +112,9 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm660
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 BOARD_RAMDISK_USE_LZ4 := true
 
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/xiaomi/sdm660_defconfig
+
 # Enable stats logging in LMKD
 TARGET_LMKD_STATS_LOG := false
 
