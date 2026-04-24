9ed357ea5be6c96f856bd88f450ea1263243becf
Tim Murray <timmurray@google.com>
2026-04-25 05:52:55 +0700

    sdm660-common: qcom-caf: display: Bump allocator priority to 100
     * The allocator can cause stalls elsewhere in the
       system if it's running at prio 120. Bump it to 100,
       the highest non-RT priority.
    
    Change-Id: Id1031b98e795686db3acddbec9aec99fb9c35b74
    [rsuntk: Do the same for gralloc3 (just in case)]

--- qcom-caf/display/gralloc/service.cpp
+++ qcom-caf/display/gralloc/service.cpp
@@ -38,7 +38,7 @@ using IQtiAllocator4 = vendor::qti::hardware::display::allocator::V4_0::IQtiAllo
 int main(int, char **) {
   android::sp<IQtiAllocator3> service3 =
       new vendor::qti::hardware::display::allocator::V3_0::implementation::QtiAllocator();
-
+  android::hardware::setMinSchedulerPolicy(service3, SCHED_NORMAL, -20);
   configureRpcThreadpool(4, true /*callerWillJoin*/);
   if (service3->registerAsService() != android::OK) {
     ALOGE("Cannot register QTI Allocator 3 service");
@@ -49,6 +49,7 @@ int main(int, char **) {
 #ifdef TARGET_USES_GRALLOC4
   android::sp<IQtiAllocator4> service4 =
       new vendor::qti::hardware::display::allocator::V4_0::implementation::QtiAllocator();
+  android::hardware::setMinSchedulerPolicy(service4, SCHED_NORMAL, -20);
   if (service4->registerAsService() != android::OK) {
     ALOGE("Cannot register QTI Allocator 4 service");
     return -EINVAL;
