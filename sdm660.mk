e5e9d964200b3d21a7b64491833f32ce579bd4fd
sabarop <sabar.pakpahan@gmail.com>
2026-07-22 02:17:19 +0700

    Revert "sdm660-common: Switch to xiaomi_sdm660 lights HAL"
    This reverts commit 1544c23c6c60091827b4764ca79b610fd8ff050e.

--- sdm660.mk
+++ sdm660.mk
@@ -318,7 +318,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi_sdm660
+    android.hardware.light-service.lineage
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
